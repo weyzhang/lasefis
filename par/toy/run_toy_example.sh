2f25114f9d300557d46f1398aef6bafe22d16fec
Bingwei Chen <chenbwei2012@gmail.com>
2017-09-07 17:14:26 +0800

    draw two lines in scripts/draw.py; update par directory structure

---
+++ par/toy/run_toy_example.sh
@@ -0,0 +1,31 @@
+# run forward simulation to obtain observed data
+QUEUE=q_x86_share
+ITERATIVE=-I
+NUM_PROCESS=1024
+bsub_job_name="ifos3d"
+exec="../../bin/ifos3d"
+make clean
+make install  MODEL=hh_toy_true.c -j4
+#mpirun -np 8 nice -19 ../bin/ifos3d ./in_and_out/ifos3d_toy_FW.json | tee ./in_and_out/ifos3D.out
+submit_cmd="/usr/sw-mpp/bin/bsub  $ITERATIVE -q  $QUEUE \
+-n $NUM_PROCESS -o bsub.out -J $bsub_job_name"
+CMD="$submit_cmd $exec
+    ./in_and_out/ifos3d_toy_FW.json | tee ./in_and_out/ifos3D.out
+    "
+eval $CMD
+
+cp model/toy.vs_it0 model/toy.vs.true
+cp model/toy.vp_it0 model/toy.vp.true
+cp model/toy.rho_it0 model/toy.rho.true
+
+
+# invert observed data using homogeneous starting model
+make clean 
+make install MODEL=hh_toy_start.c -j4
+# mpirun -np 8 nice -19 ../bin/ifos3d ./in_and_out/ifos3d_toy.json | tee ./in_and_out/ifos3D_INV.out
+submit_cmd="/usr/sw-mpp/bin/bsub  $ITERATIVE -q  $QUEUE \
+-n $NUM_PROCESS -o bsub.out -J $bsub_job_name"
+CMD="$submit_cmd $exec
+    ./in_and_out/ifos3d_toy.json | tee ./in_and_out/ifos3D_INV.out
+    "
+eval $CMD
--- scripts/draw.py
+++ scripts/draw.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
 import os
 import sys
+def create_rsf(n1, n2, n3, in_file, rsf_file):
+  crt_com='sfmath n1=%d n2=%d n3=%d output=0 > %s' % (n1, n2, n3, rsf_file)
+  sub_com='sed -i "s/in=.*/in=%s/g" %s' % (in_file, rsf_file)
+  os.popen(crt_com)
+  os.popen(sub_com)
+
 #help
 if len(sys.argv) < 3 or (len(sys.argv) == 3 and (sys.argv[2][0] != 'n' or sys.argv[2][2] != '=')) or (len(sys.argv) == 4 and (sys.argv[2][0] != 'n' or sys.argv[2][2] != '=')):
   print 'Usage: ./draw.py in_file n1=%d [n2=%d]'
@@ -9,7 +15,12 @@ if len(sys.argv) < 3 or (len(sys.argv) == 3 and (sys.argv[2][0] != 'n' or sys.ar
 
 #arguments
 prefix='toy'
+in_file=sys.argv[1]
+in_true_file='toy.vp.true'    #It needs to change!
 rsf_file=prefix+'.rsf'
+rsf_win_file=prefix+'_win.rsf'
+rsf_true_file='%s.rsf' % in_true_file
+rsf_true_win_file='%s_win.rsf' % in_true_file
 vpl_file=prefix+'.vpl'
 title="VP"
 label1="Depth"
@@ -19,13 +30,10 @@ unit2="m"
 n1=184  #y
 n2=160  #x
 n3=160  #z
-in_file=sys.argv[1]
 
 #create rsf file 
-crt_com='sfmath n1=%d n2=%d n3=%d output=0 > %s' % (n1, n2, n3, rsf_file)
-sub_com='sed -i "s/in=.*/in=%s/g" %s' % (in_file, rsf_file)
-os.popen(crt_com)
-os.popen(sub_com)
+create_rsf(n1, n2, n3, in_file, rsf_file)
+create_rsf(n1, n2, n3, in_true_file, rsf_true_file)
 
 #get average bias
 avg_com="sfattr want=mean < %s | awk '{print $3}'" % (rsf_file)
@@ -44,11 +52,14 @@ if len(sys.argv) == 3:
 elif len(sys.argv) == 4:
   index2=int(sys.argv[3][1])
   first2=int(sys.argv[3][3:])
-  win_com='sfwindow f%d=%d n%d=1 f%d=%d n%d=1 < %s' % (index1, first1, index1, index2, first2, index2, rsf_file)
-  plot_com='sfgraph title="%s" dash=0 plotfat=5  grid=y yreverse=n > %s' % (title, vpl_file)
+  win_com='sfwindow f%d=%d n%d=1 f%d=%d n%d=1 < %s > %s' % (index1, first1, index1, index2, first2, index2, rsf_file, rsf_win_file)
+  win_com_true='sfwindow f%d=%d n%d=1 f%d=%d n%d=1 < %s > %s' % (index1, first1, index1, index2, first2, index2, rsf_true_file, rsf_true_win_file)
+
+  plot_com='sfcat axis=2 %s %s | sfgraph title="%s" dash=0 plotfat=5  grid=y yreverse=n > %s' % (rsf_win_file, rsf_true_win_file, title, vpl_file)
   pen_com='sfpen %s' % (vpl_file)
-  t_com = '%s | %s' % (win_com, plot_com)
-  os.popen(t_com)
+  os.popen(win_com)
+  os.popen(win_com_true)
+  os.popen(plot_com)
 
 #sfpen
 os.popen(pen_com)
