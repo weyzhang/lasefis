1154eed3b386392e3cef2a4ba7e60aad9b89edbf
Bingwei Chen <chenbwei2012@gmail.com>
2017-09-08 01:38:41 +0800

    update toy case; update run_toy_example.sh

--- par/toy/run_toy_example.sh
+++ par/toy/run_toy_example.sh
@@ -4,12 +4,8 @@ ITERATIVE=-I
 NUM_PROCESS=8
 bsub_job_name="ifos3d"
 exec="../../bin/ifos3d"
-scons -C ../../libcseife -c
-scons -C ../../ -c
 scons -C ../../libcseife -j8
 scons -C ../../ model_scr=hh_toy_true.c -j8
-#make clean
-#make install  MODEL=hh_toy_true.c -j4
 #mpirun -np 8 nice -19 ../bin/ifos3d ./in_and_out/ifos3d_toy_FW.json | tee ./in_and_out/ifos3D.out
 submit_cmd="/usr/sw-mpp/bin/bsub  $ITERATIVE -q  $QUEUE \
 -n $NUM_PROCESS -o bsub.out -J $bsub_job_name"
@@ -24,12 +20,8 @@ cp model/toy.rho_it0 model/toy.rho.true
 
 
 # invert observed data using homogeneous starting model
-scons -C ../../libcseife -c
-scons -C ../../ -c
 scons -C ../../libcseife -j8
 scons -C ../../ model_scr=hh_toy_true.c -j8
-#make clean 
-#make install MODEL=hh_toy_start.c -j4
 # mpirun -np 8 nice -19 ../bin/ifos3d ./in_and_out/ifos3d_toy.json | tee ./in_and_out/ifos3D_INV.out
 submit_cmd="/usr/sw-mpp/bin/bsub  $ITERATIVE -q  $QUEUE \
 -n $NUM_PROCESS -o bsub.out -J $bsub_job_name"
